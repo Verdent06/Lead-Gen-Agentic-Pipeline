91bb6be4471c0a90d689aed8966c3f7ff1460d41
Vedant Desai <verdent06@gmail.com>
2026-06-11 11:49:58 -0400

    new

--- migrations/001_add_contact_columns.sql
+++
@@ -1,3 +0,0 @@
--- Run against an existing deal_sourcing_db to add contact columns next to company_name.
-ALTER TABLE target_entities ADD COLUMN IF NOT EXISTS primary_contact JSONB;
-ALTER TABLE target_entities ADD COLUMN IF NOT EXISTS all_contacts JSONB;
